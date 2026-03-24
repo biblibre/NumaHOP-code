ae27a5b1ae4dafe45db839ed13a66f6629ffc5df
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2026-03-24 15:34:18 +0100

    test-gen.sh

--- test-gen.sh
+++ test-gen.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-rm -r ./test-back
-rm -r ./test-front
+# Cleanup
+rm -r ./test-back 1>/dev/null 2>&1
+rm -r ./test-front 1>/dev/null 2>&1
 
 openapi-generator-cli generate \
     --strict-spec true \
     -i src/main/resources/openapi/openapi.yaml \
-    -c src/main/resources/openapi/config.yaml \
+    -c src/main/resources/openapi/config-back.yaml \
     -g spring \
     -o ./test-back
 
 openapi-generator-cli generate \
     --strict-spec true \
     -i src/main/resources/openapi/openapi.yaml \
-    -g typescript-angular \
+    -c src/main/resources/openapi/config-front.yaml \
+    -g javascript-closure-angular \
     -o ./test-front
